941c78f0ad3cac650a190867e804e6c77f919467
Sisun Cheng <997723678@qq.com>
2022-02-22 19:28:27 -0500

    part-2

--- exercise/part2-layer-functions/index.js
+++ exercise/part2-layer-functions/index.js
@@ -84,3 +84,53 @@ dynamically set the content of a tooltip.
 - https://leafletjs.com/reference.html#layer-bindtooltip
 
 ========== */
+
+// Step 1
+/*
+let p1;
+fetch('https://opendata.arcgis.com/api/v3/datasets/8ad76bc179cf44bd9b1c23d6f66f57d1_0/downloads/data?format=geojson&spatialRefId=4326')
+  .then(resp => resp.json())
+  .then(data => {
+    p1 = L.geoJSON(data)
+      .addTo(map);
+  });
+*/
+
+// Step 2
+/*
+let p2;
+fetch('https://opendata.arcgis.com/api/v3/datasets/8ad76bc179cf44bd9b1c23d6f66f57d1_0/downloads/data?format=geojson&spatialRefId=4326')
+  .then(resp => resp.json())
+  .then(data => {
+    p2 = L.geoJSON(data, {
+      style: function(feature) {
+        let names = feature.properties.NAME;
+        for (let i = 0; i < names.length; i++) {
+          return {color: campusColors[names]};
+        }
+
+      }})
+      .addTo(map);
+  });
+*/
+
+// Step 3
+let p3;
+fetch('https://opendata.arcgis.com/api/v3/datasets/8ad76bc179cf44bd9b1c23d6f66f57d1_0/downloads/data?format=geojson&spatialRefId=4326')
+  .then(resp => resp.json())
+  .then(data => {
+    p3 = L.geoJSON(data, {
+      style: function(feature) {
+        let names = feature.properties.NAME;
+        for (let i = 0; i < names.length; i++) {
+          return {color: campusColors[names]};
+        }
+      }})
+      .bindTooltip(function (layer) {
+        let nam = layer.feature.properties.NAME;
+        let add = layer.feature.properties.ADDRESS;
+        return `Name: ${nam} <br>Address: ${add}`; 
+      })
+      .addTo(map);
+  });
+
