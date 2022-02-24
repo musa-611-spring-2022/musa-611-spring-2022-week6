0b8e20b86d040b7559ab7f5a39f5b94985b72fab
Anran0716 <anranzheng0716@163.com>
2022-02-23 23:52:39 -0500

    HW 6

--- exercise/part2-layer-functions/index.js
+++ exercise/part2-layer-functions/index.js
@@ -83,4 +83,48 @@ dynamically set the content of a tooltip.
 
 - https://leafletjs.com/reference.html#layer-bindtooltip
 
+let p1;
+fetch(url)
+  .then(resp => resp.json())
+  .then(data => {
+    p1 = L.geoJSON(data)
+      .addTo(map);
+  });
+
+let p2;
+fetch(url)
+  .then(resp => resp.json())
+  .then(data => {
+    p2 = L.geoJSON(data,{
+      style: function (feature) {
+        let name = feature.properties.NAME;
+        return {color: campusColors[name]};
+   }}
+      )
+        .addTo(map);
+    });
+
 ========== */
+
+const url = 'https://opendata.arcgis.com/api/v3/datasets/8ad76bc179cf44bd9b1c23d6f66f57d1_0/downloads/data?format=geojson&spatialRefId=4326';
+
+let p2;
+let p3;
+fetch(url)
+  .then(resp => resp.json())
+  .then(data => {
+    p2 = L.geoJSON(data,{
+      style: function (feature) {
+        let name = feature.properties.NAME;
+        return {color: campusColors[name]};
+   }}
+      )
+    .bindTooltip(
+      function (layer) {
+        let name1 = layer.feature.properties.NAME;
+        let address = layer.feature.properties.ADDRESS;
+        return `Name: ${name1} <br>Address: ${address}`;
+      }
+    )
+        .addTo(map);
+    });
