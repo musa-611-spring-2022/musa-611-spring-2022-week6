efbd0980ae7ca1db392adb28561d51810d1bcb89
Ziyi Yang <89943485+yzy2000@users.noreply.github.com>
2022-02-23 20:40:18 -0500

    Update index.js

--- exercise/part2-layer-functions/index.js
+++ exercise/part2-layer-functions/index.js
@@ -84,3 +84,21 @@ dynamically set the content of a tooltip.
 - https://leafletjs.com/reference.html#layer-bindtooltip
 
 ========== */
+
+let url = 'https://opendata.arcgis.com/api/v3/datasets/8ad76bc179cf44bd9b1c23d6f66f57d1_0/downloads/data?format=geojson&spatialRefId=4326';
+fetch(url)
+  .then(response => response.json())
+  .then(data => {
+    L.geoJSON(data, {
+      style: feature => {
+          return { color: campusColors[name] };
+        }
+      })
+      .bindTooltip(function (layer) {
+        let name1 = layer.feature.properties.NAME;
+        let address = layer.feature.properties.ADDRESS;
+        return `Name: ${name1} <br>Address: ${address}`;
+      })
+      .addTo(map);
+  });
+
